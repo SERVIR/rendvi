5b9e74d02de3a0eaa49eec549f885f2ab3e9151d
kmarkert <kel.markert@gmail.com>
2020-01-16 16:34:20 -0600

    added function to convert the qa band values to individual bands

--- rendvi/masking.py
+++ rendvi/masking.py
@@ -23,12 +23,12 @@ class Masking:
     @staticmethod
     def applyModis(coll, coll2):
         """
-        Function to apply quality masking for the MXD09GQ datasets. Uses both 
+        Function to apply quality masking for the MXD09GQ datasets. Uses both
         """
         def _modisqa(img):
             t = ee.Date(img.get('system:time_start'))
 
-            qaflags = ee.Image(0).clip(img.geometry()).rename('qa')
+            qaflags = img.mask().select([0]).Not().rename('qa')
 
             count = ee.Number(coll2.filterDate(t, t.advance(1, 'day')).size())
             largeScale = ee.Image(coll2.filterDate(
@@ -40,12 +40,12 @@ class Masking:
                 largeScale.select('state_1km'), 2, 2, 'shadow_qa').eq(0), ee.Image(1)))
             snowMask = ee.Image(ee.Algorithms.If(count.eq(1),  Masking.extractBits(
                 largeScale.select('state_1km'), 12, 12, 'snow_qa').eq(0), ee.Image(1)))
-            sz = ee.Image(ee.Algorithms.If(
-                count.eq(1), largeScale.select('SensorZenith'), ee.Image(1)))
-            sz2 = ee.Image(ee.Algorithms.If(
-                count.eq(1), largeScale.select('SolarZenith'), ee.Image(1)))
-            szMask = sz.abs().multiply(0.01).lt(50).And(
-                sz2.abs().multiply(0.01).lt(60))
+            sensorZenith = ee.Image(ee.Algorithms.If(
+                count.gt(0), largeScale.select('SensorZenith'), ee.Image(1)))
+            solarZenith = ee.Image(ee.Algorithms.If(
+                count.gt(0), largeScale.select('SolarZenith'), ee.Image(1)))
+            zenithMask = sensorZenith.abs().multiply(0.01).lt(55).And(
+                solarZenith.abs().multiply(0.01).lt(80))
 
             # get qa band and bit flags
             qaBand = img.select('QC_250m')
@@ -57,20 +57,19 @@ class Masking:
 
             rangeMask = b1.gt(0).And(b2.gt(0))
 
-            # poor quality pixel flag = 1
-            qaflags = qaflags.where(rangeMask.And(qualityMask).eq(0), 1)
-            qaflags = qaflags.where(cloudMask.eq(0), 2)  # cloud pixel flag = 2
-            # shadow pixel flag = 3
-            qaflags = qaflags.where(shadowMask.eq(0), 3)
-            qaflags = qaflags.where(snowMask.eq(0), 4)  # snow pixel flag = 4
-            qaflags = qaflags.where(sz.eq(0), 5)  # sensor viewing flag = 5
-            qaflags = qaflags.where(sz2.eq(0), 6)  # sun zenith flag = 6
+            qaflags = qaflags.where(rangeMask.eq(0), 2) # out of range pixel flag = 1
+            qaflags = qaflags.where(qualityMask.eq(0), 3) # poor quality pixel flag = 1
+            qaflags = qaflags.where(cloudMask.eq(0), 4)  # cloud pixel flag = 2
+            qaflags = qaflags.where(shadowMask.eq(0), 5) # shadow pixel flag = 3
+            qaflags = qaflags.where(snowMask.eq(0), 6)  # snow pixel flag = 4
+            qaflags = qaflags.where(sensorZenith.abs().multiply(0.01).gte(55), 7)  # sensor viewing flag = 5
+            qaflags = qaflags.where(solarZenith.abs().multiply(0.01).gte(80), 8)  # sun zenith flag = 6
 
             finalMask = qualityMask.And(rangeMask).And(
-                cloudMask).And(shadowMask).And(snowMask).And(szMask)
+                cloudMask).And(shadowMask).And(snowMask).And(zenithMask)
 
             # get image from that date with poor data masked
-            return img.updateMask(finalMask)  # .addBands(qaflags)
+            return img.updateMask(finalMask).addBands(qaflags)
 
         out = coll.map(_modisqa)
 
@@ -83,3 +82,21 @@ class Masking:
             return
 
         return
+
+    @staticmethod
+    def qaBandToImage(img):
+        outOfRange = img.eq(2).updateMask(img.eq(2)).rename("outOfRange")
+        poorQuality = img.eq(3).updateMask(img.eq(3)).rename("poorQuality")
+        clouds = img.eq(4).updateMask(img.eq(4)).rename("clouds")
+        shadows = img.eq(5).updateMask(img.eq(5)).rename("shadows")
+        snow = img.eq(6).updateMask(img.eq(6)).rename("snow")
+        sensorZenith = img.eq(7).updateMask(img.eq(7)).rename("sensorZenith")
+        solarZenith = img.eq(8).updateMask(img.eq(8)).rename("solarZenith")
+
+        return ee.Image.cat([outOfRange,
+                             poorQuality,
+                             clouds,
+                             shadows,
+                             snow,
+                             sensorZenith,
+                             solarZenith])
