1c26191e5da44affb65780e8819f1f93e5af0e39
kmarkert <kel.markert@gmail.com>
2020-03-10 12:31:38 -0500

    adding bare bones cli

--- rendvi/cli.py
+++ rendvi/cli.py
@@ -1,7 +1,24 @@
+import ee
 import fire
+from rendvi.core import *
+from rendvi.masking import *
+from rendvi import collections
+
+def eeAuthenticate():
+
+    return
+
+def export(targetDate,eeAuth=None):
+
+    return
+
+def exportRange(startDate,endDate,):
+    return
 
 
+def exportClimatology():
+    return
 
 def main():
-    fire.Fire()
+    fire.Fire(export)
     return
