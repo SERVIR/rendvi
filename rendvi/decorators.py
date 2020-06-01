7e0d6dcd0289235db79a9bbea44528fb1212a37d
kmarkert <kel.markert@gmail.com>
2020-06-01 17:20:21 -0500

    adding function decorators that retain metadata for processing

---
+++ rendvi/decorators.py
@@ -0,0 +1,26 @@
+import ee
+import functools
+
+
+def retainTime(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # expects first element of args is img
+        # this assumption is true for 99.9% of fuctions used for ee.ImageCollection.map()
+        result = ee.Image(func(*args,**kwargs))
+        # org = 
+        return ee.Image(result)\
+            .set('system:time_start',args[0].get('system:time_start'))
+
+    return wrapper
+
+def retainMetadata(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        # expects first element of args is img
+        # this assumption is true for 99.9% of fuctions used for ee.ImageCollection.map()
+        result = ee.Image(func(*args,**kwargs))
+        return ee.Image(result.copyProperties(args[0])\
+            .set('system:time_start',args[0].get('system:time_start')))
+
+    return wrapper
