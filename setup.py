6439a65fe6f7a7916e72cc2cc66d86e4e9b16231
kmarkert <kel.markert@gmail.com>
2022-04-12 06:34:28 -0600

    adding install requires arguments for install

--- setup.py
+++ setup.py
@@ -5,19 +5,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='rendvi',
-      version='0.0.1',
-      description='Rapid Enhanced Normalaized Difference Vegetation Index (reNDVI)',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url='http://github.com/servir/rendvi',
-      packages=setuptools.find_packages(),
-      author='Kel Markert',
-      author_email='kel.markert@gmail.com',
-      license='GPLv3',
-      zip_safe=False,
-      include_package_data=False,
-      entry_points={
-          'console_scripts': [
-              'rendvi = rendvi.cli:main',
-          ],
-      },)
+    version='0.0.1',
+    description='Rapid Enhanced Normalaized Difference Vegetation Index (reNDVI)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/servir/rendvi',
+    packages=setuptools.find_packages(),
+    author='Kel Markert',
+    author_email='kel.markert@gmail.com',
+    license='GPLv3',
+    zip_safe=False,
+    include_package_data=False,
+    # entry_points={
+    #     'console_scripts': [
+    #         'rendvi = rendvi.cli:main',
+    #     ],
+    # },
+    install_requires=["earthengine-api", "fire", "pandas"]
+)
